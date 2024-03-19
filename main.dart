c79efd62796657b4b2aacaf700c7b2299f7411b1
Agape77 <agapecamille@gmail.com>
2024-03-19 11:34:52 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Camille Opisa";
+  int age = 21;
+  String school = "University of Nairobi";
+  String hobby = "Travelling";
+
+  print("My name is $name, I am $age years old, studying at $school and my hobby is $hobby.");
+}
---
+++ program2.dart
@@ -0,0 +1,15 @@
+void main() {
+  int num1 = 25;
+  int num2 = 10;
+
+  print("Addition: ${add(num1, num2)}");
+  print("Subtraction: ${subtract(num1, num2)}");
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int subtract(int a, int b) {
+  return a - b;
+}
